b1c56e6f69caedbc13fb9feeebfc6df23f2ba975
Jesse Del Greco <delgrecoj@allegheny.edu>
2017-01-08 23:42:57 -0500

    testscript

---
+++ testing.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## purpose; devel without pushing/pulling to test
+
+
+testing() {
+	local target="$HOME/.vim/vim-plug/vimscheme/colors/$1.vim"
+	rm -rf $target && cp colors/$1.vim $target;
+}
+
+estilo render
+testing vischeme
